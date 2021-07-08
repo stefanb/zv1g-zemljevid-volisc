ef6e1dfe46a672005dd844236d29b1d3526983b6
Stefan Baebler <stefan.baebler@gmail.com>
2021-07-08 11:09:09 +0200

    Set empty bilingual names instead of nulls

--- prepare.sh
+++ prepare.sh
@@ -23,7 +23,7 @@ rm zv1g-volisca-redno.geojson
 ogr2ogr zv1g-volisca-redno.geojson VDV-GURS-RPE.geojson -dialect sqlite \
  -sql "SELECT ST_Union(geometry),
 		src.VDV_UIME as 'name',
-		src.VDV_DJ as 'name_alt',
+		IFNULL(src.VDV_DJ, '') as 'name_alt',
 		SUM(src.POV_KM2) as 'pov_km2',
 		COUNT(src.VDV_ID) as 'vdv_count',
 		GROUP_CONCAT(src.VDV_ID, ', ') as vdv_ids
