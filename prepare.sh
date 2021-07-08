8768cfcad9b0502116e8e4871b45db8e81a2e48d
Stefan Baebler <stefan.baebler@gmail.com>
2021-07-08 17:22:56 +0200

    Expand abbreviated "ul." to "ulica" and "c." to "cesta" for better grouping

--- prepare.sh
+++ prepare.sh
@@ -22,14 +22,14 @@ ogr2ogr zv1g-volisca-predcasno.geojson VDV-GURS-RPE.geojson -dialect sqlite \
 rm zv1g-volisca-redno.geojson
 ogr2ogr zv1g-volisca-redno.geojson VDV-GURS-RPE.geojson -dialect sqlite \
  -sql "SELECT ST_Union(geometry),
-		src.VDV_UIME as 'name',
+		REPLACE(REPLACE(REPLACE(REPLACE(src.VDV_UIME, ' u.', ' ulica '), ' ul.', ' ulica '), ' c.', ' cesta '), '  ', ' ') as 'name',
 		IFNULL(src.VDV_DJ, '') as 'name_alt',
 		SUM(src.POV_KM2) as 'pov_km2',
 		COUNT(src.VDV_ID) as 'vdv_count',
 		GROUP_CONCAT(src.VDV_ID, ', ') as vdv_ids
 	FROM 'VDV' AS src
     WHERE src.ENOTA = 'VD'
-    GROUP BY src.VDV_UIME"\
+    GROUP BY name"\
  -nln VDV-GURS-RPE-Regular
 
 echo "  done."
